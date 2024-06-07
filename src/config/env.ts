907d76a1ae97c2ea55263f294cc888f8542f2c4f
Anuradha Fernando <anufdo@gmail.com>
2024-06-07 11:36:11 +0530

    add mysql and sqllite.

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,11 +7,11 @@ export const appEnv = {
     SECRET_KEY: process.env.SECRET_KEY,
   },
   database: {
-    DB_HOST: process.env.DB_HOST ?? "localhost",
-    DB_PORT: process.env.DB_PORT ?? "3306",
-    DB_USER: process.env.DB_USER ?? "root",
-    DB_PASSWORD: process.env.DB_PASSWORD ?? "password",
-    DB_NAME: process.env.DB_NAME ?? "crud_node_api_ts",
-    DB_DIALECT: process.env.DB_DIALECT ?? "mysql",
+    DB_HOST: process.env.DB_HOST ?? "",
+    DB_PORT: process.env.DB_PORT ?? "",
+    DB_USER: process.env.DB_USER ?? "",
+    DB_PASSWORD: process.env.DB_PASSWORD ?? "",
+    DB_NAME: process.env.DB_NAME ?? "",
+    DB_DIALECT: process.env.DB_DIALECT ?? "sqlite",
   },
 };
